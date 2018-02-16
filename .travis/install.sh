1d8cd692fcb25c12fbe7304d4723a3dbd52a04b0
Corentin Le Molgat <corentinl@google.com>
2018-02-16 17:27:58 +0100

    Full Script/Install refactor
    Have make/cmake as major dimension, then os, then native/docker

--- .travis/install.sh
+++ .travis/install.sh
@@ -2,41 +2,81 @@
 set -x
 set -e
 
-# Linux Native build using Make or CMake
-if [ "${TRAVIS_OS_NAME}" == linux ] && [ "${DISTRO}" == native ]; then
-	cd /tmp/ &&	curl -s -J -O -k -L \
-		'https://sourceforge.net/projects/swig/files/swig/swig-3.0.12/swig-3.0.12.tar.gz/download' && \
-		tar zxf swig-3.0.12.tar.gz && cd swig-3.0.12 && \
-		./configure --prefix "${HOME}"/swig/ && make && make install;
-	pyenv global system 3.6;
-fi
-
-# Linux Native build using Makefile
-if [ "${TRAVIS_OS_NAME}" == linux ] && [ "${DISTRO}" == native ] && [ "${BUILDER}" == make ]; then
-	sudo apt-get -yqq install \
-		git autoconf libtool zlib1g-dev gawk g++ curl cmake make lsb-release;
-	if [ "${LANGUAGE}" == python ];then
-		python3.6 -m pip install -q virtualenv wheel six;
-	elif [ "${LANGUAGE}" == java ];then
-		sudo apt-get -yqq install default-jdk;
-	elif [ "${LANGUAGE}" == csharp ];then
-		sudo apt-get -yqq install mono-complete;
+################
+##  MAKEFILE  ##
+################
+if [ "${BUILDER}" == make ];then
+	if [ "${TRAVIS_OS_NAME}" == linux ];then
+		if [ "${DISTRO}" == native ];then
+			# SWIG install
+			cd /tmp/ &&	curl -s -J -O -k -L \
+				'https://sourceforge.net/projects/swig/files/swig/swig-3.0.12/swig-3.0.12.tar.gz/download' && \
+				tar zxf swig-3.0.12.tar.gz && cd swig-3.0.12 && \
+				./configure --prefix "${HOME}"/swig/ && make && make install;
+			# OR-Tools prerequisite
+			sudo apt-get -yqq install \
+				git autoconf libtool zlib1g-dev gawk g++ curl cmake make lsb-release;
+			if [ "${LANGUAGE}" == python ];then
+				python3.6 -m pip install -q virtualenv wheel six;
+			elif [ "${LANGUAGE}" == java ];then
+				sudo apt-get -yqq install default-jdk;
+			elif [ "${LANGUAGE}" == csharp ];then
+				sudo apt-get -yqq install mono-complete;
+			fi
+		else
+			# Linux Docker Makefile build:
+			echo "NOT SUPPORTED"
+			exit 42
+		fi
+	elif [ "${TRAVIS_OS_NAME}" == osx ];then
+		if [ "${DISTRO}" == native ];then
+			# work around https://github.com/travis-ci/travis-ci/issues/8552
+			sudo chown -R "$(whoami)" /usr/local;
+			brew update;
+			if [ "${LANGUAGE}" != cc ]; then
+				brew install swig;
+			fi
+			if [ "${LANGUAGE}" == python ];then
+				brew install python3;
+			elif [ "${LANGUAGE}" == java ];then
+				brew cask install java;
+			elif [ "${LANGUAGE}" == csharp ];then
+				brew install mono;
+			fi
+		else
+			# MacOS Docker Makefile build:
+			echo "NOT SUPPORTED"
+			exit 42
+		fi
 	fi
 fi
 
-# OSX Native build using Makefile or CMake
-if [ "${TRAVIS_OS_NAME}" == osx ] && 	[ "${DISTRO}" == native ]; then
-	# work around https://github.com/travis-ci/travis-ci/issues/8552
-	sudo chown -R "$(whoami)" /usr/local;
-	brew update;
-	if [ "${LANGUAGE}" != cc ] || [ "${BUILDER}" == cmake ]; then
-		brew install swig;
-	fi
-	if [ "${LANGUAGE}" == python ] || [ "${BUILDER}" == cmake ];then
-		brew install python3;
-	elif [ "${LANGUAGE}" == java ];then
-		brew cask install java;
-	elif [ "${LANGUAGE}" == csharp ];then
-		brew install mono;
+#############
+##  CMAKE  ##
+#############
+if [ "${BUILDER}" == cmake ];then
+	if [ "${TRAVIS_OS_NAME}" == linux ];then
+		if [ "${DISTRO}" == native ];then
+			# SWIG install
+			cd /tmp/ &&	curl -s -J -O -k -L \
+				'https://sourceforge.net/projects/swig/files/swig/swig-3.0.12/swig-3.0.12.tar.gz/download' && \
+				tar zxf swig-3.0.12.tar.gz && cd swig-3.0.12 && \
+				./configure --prefix "${HOME}"/swig/ && make && make install;
+		else
+			# Linux Docker CMake build:
+			echo "NOTHING TO INSTALL"
+		fi
+	elif [ "${TRAVIS_OS_NAME}" == osx ];then
+		if [ "${DISTRO}" == native ];then
+			# work around https://github.com/travis-ci/travis-ci/issues/8552
+			sudo chown -R "$(whoami)" /usr/local;
+			brew update;
+			brew install swig;
+			brew install python3;
+		else
+			# MacOS Docker CMake build:
+			echo "NOT SUPPORTED"
+			exit 42
+		fi
 	fi
 fi
--- .travis/script.sh
+++ .travis/script.sh
@@ -1,53 +1,89 @@
 #!/usr/bin/env bash
+set -x
 set -e
 
-# Clone the OR-Tools repository
+# Clone the OR-Tools repository since we are an external project...
 if [ ! -d project ]; then
 	git clone --single-branch https://github.com/google/or-tools.git project;
 fi
 
-# Native build using Makefile
-if [ "${DISTRO}" == native ]; then
-	set -x
-	if [ "${TRAVIS_OS_NAME}" == linux ]; then
-		export PATH="${HOME}"/swig/bin:"${PATH}";
-		pyenv global system 3.6;
-	fi
+function checkenv() {
 	git --version;
 	clang --version || true;
 	gcc --version || true;
+	make --version
 	cmake --version || true;
 	swig -version || true;
 	python3.6 --version || true;
 	python3.6 -m pip --version || true;
-	set +x
+}
 
-	cd project;
-	if [ "${BUILDER}" == make ]; then
-		set -x
-		make --version
-		if [ "${TRAVIS_OS_NAME}" == linux ]; then
+################
+##  MAKEFILE  ##
+################
+if [ "${BUILDER}" == make ];then
+	if [ "${TRAVIS_OS_NAME}" == linux ];then
+		if [ "${DISTRO}" == native ];then
+			export PATH="${HOME}"/swig/bin:"${PATH}";
+			pyenv global system 3.6;
+			checkenv
+			cd project
 			make detect JDK_DIRECTORY="$(dirname "$(dirname "$(which javac)")")" UNIX_PYTHON_VER=3.6
+			make third_party
+			make "${LANGUAGE}"
+			make test_"${LANGUAGE}"
 		else
-			make detect UNIX_PYTHON_VER=3.6
+			# Linux Docker Makefile build:
+			echo "NOT SUPPORTED"
 		fi
-		make third_party
-		make "${LANGUAGE}"
-		make test_"${LANGUAGE}"
-	elif [ "${BUILDER}" == cmake ]; then
-		set -x
-		cmake -H. -Bbuild
-		cmake --build build --target all
-		cmake --build build --target test -- CTEST_OUTPUT_ON_FAILURE=1
+	elif [ "${TRAVIS_OS_NAME}" == osx ];then
+		if [ "${DISTRO}" == native ];then
+			checkenv
+			cd project
+			make detect UNIX_PYTHON_VER=3.6
+			make third_party
+			make "${LANGUAGE}"
+			make test_"${LANGUAGE}"
+		else
+			# MacOS Docker Makefile build:
+			echo "NOT SUPPORTED"
+		fi
 	fi
-	# Linux Docker build using CMake
-elif [ "${TRAVIS_OS_NAME}" == linux ] && [ "${BUILDER}" == cmake ]; then
-	CURRENT_DIR=$(dirname "$(readlink -f "$0")")
-	MAKE="make -f ${CURRENT_DIR}/Makefile"
-	set -x
-	${MAKE} print-ROOT_DIR
-	${MAKE} docker_"${DISTRO}"
-	${MAKE} configure_"${DISTRO}"
-	${MAKE} build_"${DISTRO}"
-	${MAKE} test_"${DISTRO}"
+fi
+
+#############
+##  CMAKE  ##
+#############
+if [ "${BUILDER}" == cmake ];then
+	if [ "${TRAVIS_OS_NAME}" == linux ];then
+		if [ "${DISTRO}" == native ];then
+			export PATH="${HOME}"/swig/bin:"${PATH}";
+			pyenv global system 3.6;
+			checkenv
+			cd project
+			cmake -H. -Bbuild
+			cmake --build build --target all
+			cmake --build build --target test -- CTEST_OUTPUT_ON_FAILURE=1
+		else
+			# We use the .travis/Makefile which orchestrate the Step-by-Step
+			# note: we could also only call the last target since dependencies are OK.
+			CURRENT_DIR=$(dirname "$(readlink -f "$0")")
+			MAKE="make -f ${CURRENT_DIR}/Makefile"
+			${MAKE} docker_"${DISTRO}"
+			${MAKE} configure_"${DISTRO}"
+			${MAKE} build_"${DISTRO}"
+			${MAKE} test_"${DISTRO}"
+		fi
+	elif [ "${TRAVIS_OS_NAME}" == osx ];then
+		if [ "${DISTRO}" == native ];then
+			checkenv
+			cd project
+			cmake -H. -Bbuild
+			cmake --build build --target all
+			cmake --build build --target test -- CTEST_OUTPUT_ON_FAILURE=1
+		else
+			# MacOS Docker Makefile build:
+			echo "NOT SUPPORTED"
+		fi
+	fi
 fi
